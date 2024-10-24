12aebb557a65f45dc75981711918b689c0ce0324
Ash Entwisle <ash.entwisle@protonmail.com>
2024-10-24 14:06:30 +0100

    added debug comments to help me out...
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,17 +4,28 @@
 DIR_SRC="/root/src"
 DIR_LOCAL="/root/local"
 
+echo "start..."
+
 # cd to src
 cd "$DIR_SRC"
+
+echo "cd successful"
 
 # Fetch the latest changes from the remote
 git fetch
 
+echo "fetch successful"
+
 # Get the current branch name
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
+echo "branch success"
+
 # Check if the current branch is behind the remote
 behind=$(git rev-list --count "${current_branch}".."origin/${current_branch}")
+
+echo "behind success"
+
 
 if [ "$behind" -gt 0 ]; then
   echo "Updates available for '$current_branch'. Pulling changes..."
