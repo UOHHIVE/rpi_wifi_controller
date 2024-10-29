d12125573ee9ab6e01dbc0dfcc81c496fd225aa2
Ash Entwisle <ash.entwisle@protonmail.com>
2024-10-29 11:21:49 +0000

    added more script stuff
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -12,6 +12,29 @@ if [ ! -d "$DIR_SRC" ]; then mkdir $DIR_SRC; fi
 # clone repo to src
 git clone "$DIR_REM" "$DIR_SRC" --recurse-submodules
 
-# run setup
-chmod +x "$DIR_SRC/scripts/setup.sh"
-bash "$DIR_SRC/scripts/setup.sh"
+# cd to repo and pull
+cd "$DIR_SRC"
+git pull
+
+# setup the folders and build the project
+make setup
+make build
+
+# if target dir isnt linked, then link it
+if [ ! -d "$DIR_LOCAL" ]; then ln -s "$DIR_SRC/target" "$DIR_LOCAL"; fi
+
+cd "$DIR_LOCAL"
+
+# start and enable rpi_controller.service and rpi_updater.service
+for service_file in "$DIR_SRC"/scripts/*.service; do
+    if [ -f "$service_file" ]; then
+        service_name=$(basename "$service_file")
+
+        ln -sf "$service_file" "/etc/systemd/system/$service_name"
+
+        systemctl stop "$service_name"
+        systemctl enable "$service_name"
+        systemctl start "$service_name"
+    fi
+done
+
