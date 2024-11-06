4f48f80cb1e9a6a5f42bb8032e1e4871d83d3343
Ash Entwisle <ash.entwisle@protonmail.com>
2024-11-06 15:34:39 +0000

    testing motor commands
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <thread>
 #include <wiringPi.h> // Include WiringPi library!
 
+#define TRACK_L 17 // Left Track pin
+#define TRACK_R 27 // Right Track Pin
+#define SAFETY 16  // Safety Pin
+#define HBREAK 26  // Break Pin
+
 // URL:
 // https://stackoverflow.com/questions/158585/how-do-you-add-a-timed-delay-to-a-c-program
 using namespace std::this_thread;     // sleep_for, sleep_until
@@ -17,34 +22,69 @@ void blink(int pin) {
   delay(100);
 }
 
+void start() { digitalWrite(HBREAK, HIGH); }
+
+void stop() { digitalWrite(HBREAK, LOW); }
+
+void go() {
+  digitalWrite(TRACK_L, HIGH);
+  digitalWrite(TRACK_R, HIGH);
+}
+
+void turn_left() {
+  digitalWrite(TRACK_L, HIGH);
+  digitalWrite(TRACK_R, LOW);
+}
+
+void turn_right() {
+  digitalWrite(TRACK_L, LOW);
+  digitalWrite(TRACK_R, HIGH);
+}
+
+void clear() {
+  sleep_for(0.5s);
+  digitalWrite(TRACK_L, LOW);
+  digitalWrite(TRACK_R, LOW);
+  digitalWrite(HBREAK, LOW);
+  // digitalWrite(SAFETY, LOW);
+}
+
 int main(void) {
+
   // uses BCM numbering of the GPIOs and directly accesses the GPIO registers.
   wiringPiSetupGpio();
 
-  // // pin mode ..(INPUT, OUTPUT, PWM_OUTPUT, GPIO_CLOCK)
-  // // set pin 17 to input
-  // pinMode(17, OUTPUT);
-
-  // // pull up/down mode (PUD_OFF, PUD_UP, PUD_DOWN) => down
-  // pullUpDnControl(17, PUD_DOWN);
-
-  // // get state of pin 17
-  // int value = digitalRead(17);
-
-  // if (HIGH == value) {
-  //   // your code
-  // }
-
+  // Initialise pins
   pinMode(17, OUTPUT);
   pinMode(27, OUTPUT);
   pinMode(16, OUTPUT);
   pinMode(26, OUTPUT);
 
+  digitalWrite(SAFETY, HIGH);
+
+  // blink lights in sequence
   for (;;) {
     blink(17);
     blink(27);
     blink(16);
     blink(26);
+
+    clear();
+
+    start();
+    clear();
+
+    stop();
+    clear();
+
+    go();
+    clear();
+
+    turn_left();
+    clear();
+
+    turn_right();
+    clear();
   }
   return 0;
 }
