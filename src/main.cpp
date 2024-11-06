d06b93ab093af03afcb8c0595319c34d2660fefc
Ash Entwisle <ash.entwisle@protonmail.com>
2024-11-06 16:35:51 +0000

    reorganised
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "zumo.h"
 #include <chrono>
 #include <stdio.h>
 #include <thread>
@@ -14,109 +15,50 @@ using namespace std::this_thread;     // sleep_for, sleep_until
 using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 using std::chrono::system_clock;
 
-void blink(int pin) {
-  printf("blinking %d", pin);
-  digitalWrite(pin, HIGH);
-  delay(400);
-  digitalWrite(pin, LOW);
-  delay(100);
-}
-
-void start() { digitalWrite(HBREAK, HIGH); }
-
-void stop() { digitalWrite(HBREAK, LOW); }
-
-void forward() {
-  digitalWrite(TRACK_L, HIGH);
-  digitalWrite(TRACK_R, HIGH);
-}
-
-void turn_left() {
-  digitalWrite(TRACK_L, HIGH);
-  digitalWrite(TRACK_R, LOW);
-}
-
-void turn_right() {
-  digitalWrite(TRACK_L, LOW);
-  digitalWrite(TRACK_R, HIGH);
-}
-
-void clear() {
-  sleep_for(0.8s);
-  digitalWrite(TRACK_L, LOW);
-  digitalWrite(TRACK_R, LOW);
-  digitalWrite(HBREAK, LOW);
-  // digitalWrite(SAFETY, LOW);
-  sleep_for(0.2s);
-}
-
 int main(void) {
 
-  // uses BCM numbering of the GPIOs and directly accesses the GPIO registers.
-  wiringPiSetupGpio();
+  zumo_utils::setup();
 
-  // Initialise pins
-  pinMode(17, OUTPUT);
-  pinMode(27, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(26, OUTPUT);
-
-  digitalWrite(17, HIGH);
-  digitalWrite(27, HIGH);
-  digitalWrite(16, HIGH);
-  digitalWrite(26, HIGH);
+  zumo_utils::block();
 
   sleep_for(2s);
 
-  digitalWrite(17, LOW);
-  digitalWrite(27, LOW);
-  digitalWrite(16, LOW);
-  digitalWrite(26, LOW);
+  zumo_utils::clear();
 
   sleep_for(0.5s);
 
-  digitalWrite(17, HIGH);
-  digitalWrite(27, HIGH);
-  digitalWrite(16, HIGH);
-  digitalWrite(26, HIGH);
+  zumo_utils::block();
 
   sleep_for(2s);
 
-  digitalWrite(17, LOW);
-  digitalWrite(27, LOW);
-  digitalWrite(16, LOW);
-  digitalWrite(26, LOW);
+  zumo_utils::clear();
 
-  sleep_for(2s);
+  sleep_for(1s);
 
   digitalWrite(SAFETY, HIGH);
 
   // blink lights in sequence
   for (;;) {
-    // blink(17);
-    // blink(27);
-    // blink(16);
-    // blink(26);
 
-    clear();
+    zumo_utils::clear();
 
-    start();
-    clear();
+    zumo_movement::start();
+    zumo_utils::clear();
 
-    stop();
-    clear();
+    zumo_movement::stop();
+    zumo_utils::clear();
 
-    forward();
-    start();
-    clear();
+    zumo_movement::forward();
+    zumo_movement::start();
+    zumo_utils::clear();
 
-    turn_left();
-    start();
-    clear();
+    zumo_movement::turn_left();
+    zumo_movement::start();
+    zumo_utils::clear();
 
-    turn_right();
-    start();
-    clear();
+    zumo_movement::turn_right();
+    zumo_movement::start();
+    zumo_utils::clear();
   }
   return 0;
 }
