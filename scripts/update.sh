626c51b73bc5ba94ce83e1752f3e541e94d666be
Ash Entwisle <ash.entwisle@protonmail.com>
2024-11-07 15:51:53 +0000

    added submodule updarer to code
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -22,6 +22,7 @@ behind=$(git rev-list --count "${current_branch}".."origin/${current_branch}")
 if [ "$behind" -gt 0 ]; then
   echo "Updates available for '$current_branch'. Pulling changes..."
   git pull origin "$current_branch"
+  git submodule update --init --recursive --remote
   bash "$DIR_LOCAL/setup.sh"
 else
     echo "Branch '$current_branch' is up to date."
