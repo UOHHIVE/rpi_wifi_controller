70f59d52fd0859add67fb05a566313b206912add
Ash Entwisle <ash.entwisle@protonmail.com>
2024-11-18 16:46:13 +0000

    removed testnig stuff
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,8 @@ static bool test_bool = true;
 void tcp_listener() {
   printf("started listening...\n");
   while (1) {
-    // TODO: add code to set up the listener stuff
+    // TODO: sort out magic number connections
+    // TODO: start listening
 
     std::lock_guard<std::mutex> lock(STATE.mtx);
     STATE.inner += 1;
@@ -40,53 +41,38 @@ void tcp_listener() {
   }
 }
 
-void netcode_test() {
-
-  ConnManager::cm_connect("10.140.10.31", 6009);
-
-  char *hello = "Hello from client";
-  ConnManager::cm_write(hello);
-
-  ConnManager::cm_read(BUFFER);
-  printf("BUFFER: %s\n", BUFFER);
-
-  ConnManager::cm_disconnect();
-}
-
 int main(void) {
 
   std::thread p_listener(tcp_listener);
-  std::thread p_test(netcode_test);
 
   // define timing stuff
-  // std::chrono::_V2::system_clock::duration p1;
-  // std::chrono::_V2::system_clock::duration p2;
+  std::chrono::_V2::system_clock::duration p1;
+  std::chrono::_V2::system_clock::duration p2;
 
-  // int64_t t1;
-  // int64_t t2;
+  int64_t t1;
+  int64_t t2;
 
-  // int t_delay;
+  int t_delay;
 
-  // while (true) {
+  while (true) {
 
-  //   p1 = std::chrono::high_resolution_clock::now().time_since_epoch();
-  //   t1 = std::chrono::duration_cast<std::chrono::microseconds>(p1).count();
+    p1 = std::chrono::high_resolution_clock::now().time_since_epoch();
+    t1 = std::chrono::duration_cast<std::chrono::microseconds>(p1).count();
 
-  //   // TODO: read state
-  //   // TODO: compute state
-  //   // TODO: do movement stuff...
+    // TODO: read state
+    // TODO: compute state
+    // TODO: do movement stuff...
 
-  //   printf("tick \n");
-  //   printf("state=%d (tick delay: %d)\n", STATE.read(), t_delay);
+    printf("tick \n");
+    printf("state=%d (tick delay: %d)\n", STATE.read(), t_delay);
 
-  //   p2 = std::chrono::high_resolution_clock::now().time_since_epoch();
-  //   t2 = std::chrono::duration_cast<std::chrono::microseconds>(p2).count();
+    p2 = std::chrono::high_resolution_clock::now().time_since_epoch();
+    t2 = std::chrono::duration_cast<std::chrono::microseconds>(p2).count();
 
-  //   t_delay = MSPT - (t2 - t1);
-  //   std::this_thread::sleep_for(std::chrono::microseconds(t_delay));
-  // }
+    t_delay = MSPT - (t2 - t1);
+    std::this_thread::sleep_for(std::chrono::microseconds(t_delay));
+  }
 
   p_listener.join();
-  p_test.join();
   return 0;
 }
