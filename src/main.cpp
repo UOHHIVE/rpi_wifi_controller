b9aaa6b642b9a21a5714b746097f1f241b9127d1
Ash Entwisle <ash.entwisle@protonmail.com>
2024-11-28 11:16:37 +0000

    Added registration string
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 // #include "commons/src/dotenv/dotenv.cpp"
 #include "commons/hive_commons.hpp"
 #include <chrono>
+#include <memory>
 #include <stdio.h>
 #include <sys/time.h>
 #include <thread>
+#include <type_traits>
+
+#include "commons/src/flatbuf/commons_generated.h"
+#include <utility>
 
 // URL:
 // https://stackoverflow.com/questions/158585/how-do-you-add-a-timed-delay-to-a-c-program
@@ -23,6 +28,10 @@ using namespace dotenv;
 static Lock<int> STATE;
 static char BUFFER[1024];
 
+inline uint16_t encodeSubscriptionType(const HiveCommon::SubscriptionType type, const uint16_t subscription = 0) {
+  return static_cast<uint16_t>(1 << std::to_underlying(type)) | subscription;
+}
+
 void tcp_listener() {
 
   printf("started listening...\n");
@@ -31,9 +40,34 @@ void tcp_listener() {
   string dc_port = DotEnv::get("DC_PORT");
   Socket s = Socket(dc_address.data(), std::stoi(dc_port));
 
-  // TODO: construct subscriber fb
-  // char *hello = "Hello from client";
-  // s._send(hello);
+  string name = DotEnv::get("BOT_NAME");
+  uint64_t id = std::stoll(DotEnv::get("ID_OVERRIDE")); // TODO: this borked when using hex
+
+  // Build the Presenter which is to be used in the Payload as a byte vector
+  uint16_t sub = 0;
+
+  // sub = encodeSubscriptionType(HiveCommon::SubscriptionType_Headset, sub);
+  sub = encodeSubscriptionType(HiveCommon::SubscriptionType_Own, sub);
+  flatbuffers::FlatBufferBuilder fbb1;
+  const auto fb_name = fbb1.CreateString(name);
+  const auto robot = HiveCommon::CreateRobot(fbb1, id, fb_name, sub, HiveCommon::SubscriptionRate_Half);
+  const auto entity = HiveCommon::CreateEntity(fbb1, HiveCommon::EntityUnion_Robot, robot.Union());
+  fbb1.Finish(entity);
+  fbb1.ForceVectorAlignment(fbb1.GetSize(), sizeof(uint8_t), fbb1.GetBufferMinAlignment());
+
+  // Build the Payload which is to be used in the State as a payload vector
+  flatbuffers::FlatBufferBuilder fbb2;
+  const auto entityVec = fbb2.CreateVector(fbb1.GetBufferPointer(), fbb1.GetSize());
+  const auto payload = HiveCommon::CreatePayload(fbb2, entityVec);
+
+  std::vector<flatbuffers::Offset<HiveCommon::Payload>> payloadVector;
+  payloadVector.push_back(payload);
+  const auto payloads = fbb2.CreateVector(payloadVector);
+
+  const auto state = HiveCommon::CreateState(fbb2, payloads);
+  fbb2.FinishSizePrefixed(state);
+
+  s.send_data(reinterpret_cast<char *>(fbb2.GetBufferPointer()), fbb2.GetSize());
 
   while (1) {
     // TODO: start listening
