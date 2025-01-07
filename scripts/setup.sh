5cfaa9c04159be9e8ed2434d4ef721ab5f40351d
Ash Entwisle <ash.entwisle@protonmail.com>
2025-01-07 10:03:02 +0000

    added setup script that i removed for some reason...
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

---
+++ scripts/setup.sh
@@ -0,0 +1,37 @@
+
+# set vars
+DIR_REM="https://github.com/UoH-HIVE/raspberry_pi_wifi_controller.git"
+DIR_SRC="/root/src"
+DIR_LOCAL="/root/local"
+
+# if /usr/src/hive_rpi/ doesnt exist, make it
+if [ ! -d "$DIR_SRC" ]; then mkdir $DIR_SRC; fi
+
+# clone repo to src
+git clone "$DIR_REM" "$DIR_SRC" --recurse-submodules
+
+# cd to repo and pull
+cd "$DIR_SRC"
+git pull
+
+# setup the folders and build the project
+make setup
+make build
+
+# if target dir isnt linked, then link it
+if [ ! -d "$DIR_LOCAL" ]; then ln -s "$DIR_SRC/target" "$DIR_LOCAL"; fi
+
+cd "$DIR_LOCAL"
+
+# start and enable rpi_controller.service and rpi_updater.service
+for service_file in "$DIR_SRC"/scripts/*.service; do
+    if [ -f "$service_file" ]; then
+        service_name=$(basename "$service_file")
+
+        ln -sf "$service_file" "/etc/systemd/system/$service_name"
+
+        systemctl stop "$service_name"
+        systemctl enable "$service_name"
+        systemctl start "$service_name"
+    fi
+done
