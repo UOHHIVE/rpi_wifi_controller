4606b83adb351c6d9ca6ce801df879e0fb912710
Ash Entwisle <ash.entwisle@protonmail.com>
2025-03-03 09:50:02 +0000

    updated install script with all functionality
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -50,3 +50,46 @@ apt install ./wiringpi*.deb
 # post install clean
 cd .. 
 rm -rf WiringPi/
+
+
+# if /usr/src/hive_rpi/ doesnt exist, make it
+if [ ! -d "$DIR_SRC" ]; then mkdir $DIR_SRC; fi
+
+# clone repo to src
+if [ -z "$NO_CLONE" ]; then git clone "$DIR_REM" "$DIR_SRC"; fi
+
+# cd to repo and pull
+cd "$DIR_SRC"
+git pull origin main
+
+# check if target dir exists
+if [ ! -d "$DIR_TARGET" ]; then mkdir "$DIR_TARGET"; fi
+
+# unzip target tarball into target dir
+rm -rf "$DIR_TARGET"/*
+tar -xvf target.tar.gz -C "$DIR_SRC"
+
+# if target dir isnt linked, then link it
+if [ ! -d "$DIR_LOCAL" ]; then ln -s "$DIR_TARGET" "$DIR_LOCAL"; fi
+
+# copy config file to root if it doesnt exist
+if [ ! -f /root/config.env ]; then cp /root/local/config.env /root/config.env; fi
+
+# move into local dir
+cd "$DIR_LOCAL"
+
+# start and enable rpi_controller.service and rpi_updater.service
+for service_file in "$DIR_SRC"/scripts/*.service; do
+    if [ -f "$service_file" ]; then
+        service_name=$(basename "$service_file")
+
+        ln -sf "$service_file" "/etc/systemd/system/$service_name"
+
+        systemctl stop "$service_name"
+        systemctl enable "$service_name"
+        systemctl start "$service_name"
+    fi
+done
+
+# if not NO_SLEEP, sleep for 30 seconds
+if [ -z "$NO_SLEEP" ]; then sleep 30; fi
