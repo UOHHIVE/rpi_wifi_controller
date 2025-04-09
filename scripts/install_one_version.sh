e3c3a174176349cff595d0a833fb850ad0417b58
Ash <ash.entwisle@protonmail.com>
2025-04-09 09:26:09 +0100

    Update install_one_version.sh

--- scripts/install_one_version.sh
+++ scripts/install_one_version.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # set vars
-DIR_REM="https://github.com/UoH-HIVE/rpi_wifi_controller/raw/refs/heads/main/target.tar.gz"
+DIR_REM="https://github.com/UOHHIVE/rpi_wifi_controller/raw/refs/heads/main/target.tar.gz"
 DIR_SRC="/root/src"
 DIR_TARGET="$DIR_SRC/target"
 DIR_LOCAL="/root/local"
